65ba268f16447816e00de8e8ebfccfca46bfe3ce
imzhang95 <imzhang95@users.noreply.github.com>
2025-05-26 01:16:23 +0800

    update style

--- src/ServerRow.tsx
+++ src/ServerRow.tsx
@@ -195,7 +195,7 @@ const ServerRow: React.FC<SergateData> = (props: SergateData) => {
           <Col xs={3} sm={3} md={1} lg={1}>{onlineTag(server.online4, 'IPv4')}</Col>
           <Col xs={0} sm={0} md={1} lg={1}>{onlineTag(server.online6, 'IPv6')}</Col>
           <Col xs={5} sm={4} md={2} lg={2}>
-            <span style={{border: isNearExpiry(server) ? '1px dashed #8B0000' : 'none'}}>
+            <span style={{border: isNearExpiry(server) ? '2px dotted #8B0000' : 'none'}}>
               {server.alias || server.name}
             </span>
           </Col>
